e154983df4156f78c52cb2b03289de65bef274c3
Roberts Simanis <robsimanis@gmail.com>
2019-11-23 15:34:46 +0200

    first database test

--- app/src/main/java/com/enelondroid/controlstuff/MainActivity.java
+++ app/src/main/java/com/enelondroid/controlstuff/MainActivity.java
@@ -2,12 +2,40 @@ package com.enelondroid.controlstuff;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button but1, but2;
+    private FirebaseDatabase database;
+    private DatabaseReference nodemcu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        but1 = findViewById(R.id.button1);
+        but2 = findViewById(R.id.button2);
+
+        database = FirebaseDatabase.getInstance();
+        nodemcu = database.getReference("nodemcu");
+
+        but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nodemcu.setValue(1);
+            }
+        });
+
+        but2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nodemcu.setValue(2);
+            }
+        });
     }
 }
