c5892f21bec5d296be7799b43266f14ab06aae03
Roberts Simanis <robsimanis@gmail.com>
2019-11-23 16:20:37 +0200

    add laser controller

--- app/src/main/java/com/enelondroid/controlstuff/MainActivity.java
+++ app/src/main/java/com/enelondroid/controlstuff/MainActivity.java
@@ -1,40 +1,67 @@
 package com.enelondroid.controlstuff;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button but1, but2;
+    private ImageButton laserbut;
     private FirebaseDatabase database;
     private DatabaseReference nodemcu;
+
+    boolean isOn = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        but1 = findViewById(R.id.button1);
-        but2 = findViewById(R.id.button2);
+        laserbut = findViewById(R.id.laserbutton);
 
         database = FirebaseDatabase.getInstance();
         nodemcu = database.getReference("nodemcu");
 
-        but1.setOnClickListener(new View.OnClickListener() {
+
+        nodemcu.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                nodemcu.setValue(1);
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (String.valueOf(dataSnapshot.getValue()).equals("1")) {
+                    isOn = false;
+                    laserbut.setColorFilter(null);
+                } else {
+                    isOn = true;
+                    laserbut.setColorFilter(Color.rgb(240,255,0));
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, "Synchronization error",
+                        Toast.LENGTH_LONG).show();
             }
         });
-
-        but2.setOnClickListener(new View.OnClickListener() {
+        laserbut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                nodemcu.setValue(2);
+                if (isOn) {
+                    nodemcu.setValue(1);
+                    isOn = false;
+                    laserbut.setColorFilter(null);
+                } else {
+                    nodemcu.setValue(2);
+                    isOn = true;
+                    laserbut.setColorFilter(Color.rgb(240,255,0));
+                }
             }
         });
     }
