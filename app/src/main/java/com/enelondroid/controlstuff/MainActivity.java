65565c6e5034ff117cab44f5b4a677a0cf6fd690
Roberts Simanis <robsimanis@gmail.com>
2020-06-08 14:42:08 +0300

    Turned into attentinator

--- app/src/main/java/com/enelondroid/controlstuff/MainActivity.java
+++ app/src/main/java/com/enelondroid/controlstuff/MainActivity.java
@@ -17,34 +17,32 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton laserbut, starsbut;
+    private ImageButton attentibut;
     private FirebaseDatabase database;
-    private DatabaseReference nodemcu, esp82661;
+    private DatabaseReference attentinator;
 
     boolean isOn = false;
-    boolean isStarsOn = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        laserbut = findViewById(R.id.laserbutton);
-        starsbut = findViewById(R.id.starsbutton);
+        attentibut = findViewById(R.id.attentibutton);
 
         database = FirebaseDatabase.getInstance();
-        nodemcu = database.getReference("nodemcu");
-        esp82661 = database.getReference("esp82661");
+        attentinator = database.getReference("attentinator");
 
 
-        nodemcu.addValueEventListener(new ValueEventListener() {
+        attentinator.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (String.valueOf(dataSnapshot.getValue()).equals("1")) {
                     isOn = false;
-                    laserbut.setColorFilter(null);
+                    attentibut.setColorFilter(null);
                 } else {
                     isOn = true;
-                    laserbut.setColorFilter(Color.rgb(240,255,0));
+                    attentibut.setColorFilter(Color.rgb(240,255,0));
                 }
             }
             @Override
@@ -54,52 +52,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        esp82661.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (String.valueOf(dataSnapshot.getValue()).equals("2")) {
-                    isStarsOn = false;
-                    starsbut.setColorFilter(null);
-                } else {
-                    isStarsOn = true;
-                    starsbut.setColorFilter(Color.rgb(240,255,0));
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Toast.makeText(MainActivity.this, "Synchronization error",
-                        Toast.LENGTH_LONG).show();
-            }
-        });
-
-        laserbut.setOnClickListener(new View.OnClickListener() {
+        attentibut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (isOn) {
-                    nodemcu.setValue(1);
+                    attentinator.setValue(1);
                     isOn = false;
-                    laserbut.setColorFilter(null);
+                    attentibut.setColorFilter(null);
                 } else {
-                    nodemcu.setValue(2);
+                    attentinator.setValue(2);
                     isOn = true;
-                    laserbut.setColorFilter(Color.rgb(240,255,0));
-                }
-            }
-        });
-
-        starsbut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (isStarsOn) {
-                    esp82661.setValue(2);
-                    isStarsOn = false;
-                    starsbut.setColorFilter(null);
-                } else {
-                    esp82661.setValue(1);
-                    isStarsOn = true;
-                    starsbut.setColorFilter(Color.rgb(240,255,0));
+                    attentibut.setColorFilter(Color.rgb(240,255,0));
                 }
             }
         });
